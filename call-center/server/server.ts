c391a3a11a52244a23dfd07b61187e8f53cc6082
Hugo Bessa <eu@hugobessa.com.br>
2020-08-25 11:58:01 -0300

    Add working simple express server

--- call-center/server/server.ts
+++ call-center/server/server.ts
@@ -1,1 +1,11 @@
 let express = require('express');
+
+let app: express.Application = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello world');
+});
+
+app.listen(3000, function () {
+  console.log(`App is listening on post 3000`);
+});
