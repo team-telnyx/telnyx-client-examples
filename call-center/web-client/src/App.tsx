9981fe06383112c3d25789dfc68a3ec60f4c2124
Sua Yoo <sua@telnyx.com>
2020-09-02 15:19:42 -0700

    add basic markup

--- call-center/web-client/src/App.tsx
+++ call-center/web-client/src/App.tsx
@@ -5,20 +5,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Call Center Login</h1>
       </header>
+
+      <main>
+        <form>
+          <label htmlFor="display_name_input">Name</label>
+          <div>
+            <input id="display_name_input" name="display_name" type="text" />
+            <button type="submit">Login</button>
+          </div>
+        </form>
+      </main>
     </div>
   );
 }
