f266faf238b28986de489bbd57fcaf04b5ab198f
Sua Yoo <sua@telnyx.com>
2020-09-02 15:50:22 -0700

    add additional markup

--- call-center/web-client/src/App.tsx
+++ call-center/web-client/src/App.tsx
@@ -12,10 +12,97 @@ function App() {
         <form>
           <label htmlFor="display_name_input">Name</label>
           <div>
-            <input id="display_name_input" name="display_name" type="text" />
-            <button type="submit">Login</button>
+            <input
+              id="display_name_input"
+              className="App-form-input"
+              name="display_name"
+              type="text"
+            />
+            <button type="submit" className="App-button">
+              Login
+            </button>
           </div>
         </form>
+
+        <section>
+          <h2>Other available agents</h2>
+
+          <ul>
+            <li>
+              <div>Agent Name</div>
+            </li>
+            <li>
+              <div>Agent Name</div>
+            </li>
+            <li>
+              <div>Agent Name</div>
+            </li>
+          </ul>
+        </section>
+
+        <form>
+          <label htmlFor="phone_number_input">Phone number</label>
+          <div>
+            <input
+              id="phone_number_input"
+              className="App-form-input"
+              name="phone_number"
+              type="text"
+            />
+            <button type="submit" className="App-button">
+              Call
+            </button>
+          </div>
+        </form>
+
+        <section>
+          <div>
+            <div>Incoming call</div>
+            <div>+13125551111</div>
+            <div>
+              <button type="button" className="App-button">
+                Answer
+              </button>
+              <button type="button" className="App-button">
+                Hangup
+              </button>
+            </div>
+          </div>
+
+          <div>
+            <div>Call in progress</div>
+            <div>+13125551111</div>
+            <div>
+              <button type="button" className="App-button">
+                Hangup
+              </button>
+              <button type="button" className="App-button">
+                Mute
+              </button>
+            </div>
+          </div>
+
+          <div>
+            <div>Conference call in progress</div>
+            <div>+13125551111</div>
+            <div>+ Agent Name</div>
+            <div>+ Agent Name</div>
+            <div>
+              <button type="button" className="App-button">
+                Hangup
+              </button>
+              <button type="button" className="App-button">
+                Mute
+              </button>
+            </div>
+          </div>
+        </section>
+
+        <footer>
+          <button type="button" className="App-button">
+            Logout
+          </button>
+        </footer>
       </main>
     </div>
   );
