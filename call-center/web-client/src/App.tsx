bad1f9b65713834e007ab28fe567cb15ee9b636c
Sua Yoo <sua@telnyx.com>
2020-09-02 16:10:03 -0700

    add button variants

--- call-center/web-client/src/App.tsx
+++ call-center/web-client/src/App.tsx
@@ -9,89 +9,135 @@ function App() {
       </header>
 
       <main>
-        <form>
-          <label htmlFor="display_name_input">Name</label>
-          <div>
-            <input
-              id="display_name_input"
-              className="App-form-input"
-              name="display_name"
-              type="text"
-            />
-            <button type="submit" className="App-button">
-              Login
-            </button>
-          </div>
-        </form>
+        <section className="App-section">
+          <form>
+            <label htmlFor="display_name_input">Name</label>
+            <div>
+              <input
+                id="display_name_input"
+                className="App-input"
+                name="display_name"
+                type="text"
+              />
+              <button type="submit" className="App-button App-button--primary">
+                Login
+              </button>
+            </div>
+          </form>
+        </section>
 
-        <section>
+        <section className="App-section">
           <h2>Other available agents</h2>
 
           <ul>
             <li>
               <div>Agent Name</div>
+              <div>
+                <button
+                  type="button"
+                  className="App-button App-button--secondary"
+                >
+                  Transfer
+                </button>
+                <button
+                  type="button"
+                  className="App-button App-button--secondary"
+                >
+                  Add
+                </button>
+              </div>
             </li>
             <li>
               <div>Agent Name</div>
+              <div>
+                <button
+                  type="button"
+                  className="App-button App-button--secondary"
+                >
+                  Transfer
+                </button>
+                <button
+                  type="button"
+                  className="App-button App-button--secondary"
+                >
+                  Add
+                </button>
+              </div>
             </li>
             <li>
               <div>Agent Name</div>
+              <div>
+                <button
+                  type="button"
+                  className="App-button App-button--secondary"
+                >
+                  Transfer
+                </button>
+                <button
+                  type="button"
+                  className="App-button App-button--secondary"
+                >
+                  Add
+                </button>
+              </div>
             </li>
           </ul>
         </section>
 
-        <form>
-          <label htmlFor="phone_number_input">Phone number</label>
-          <div>
-            <input
-              id="phone_number_input"
-              className="App-form-input"
-              name="phone_number"
-              type="text"
-            />
-            <button type="submit" className="App-button">
-              Call
-            </button>
-          </div>
-        </form>
+        <section className="App-section">
+          <form>
+            <label htmlFor="phone_number_input">Phone number</label>
+            <div>
+              <input
+                id="phone_number_input"
+                className="App-input"
+                name="phone_number"
+                type="text"
+              />
+              <button type="submit" className="App-button App-button--primary">
+                Call
+              </button>
+            </div>
+          </form>
+        </section>
 
         <section>
-          <div>
+          <div className="App-section">
             <div>Incoming call</div>
             <div>+13125551111</div>
             <div>
-              <button type="button" className="App-button">
+              <button type="button" className="App-button App-button--primary">
                 Answer
               </button>
-              <button type="button" className="App-button">
+              <button type="button" className="App-button App-button--danger">
                 Hangup
               </button>
             </div>
           </div>
 
-          <div>
+          <div className="App-section">
             <div>Call in progress</div>
             <div>+13125551111</div>
             <div>
-              <button type="button" className="App-button">
+              <button type="button" className="App-button App-button--danger">
                 Hangup
               </button>
-              <button type="button" className="App-button">
+              <button type="button" className="App-button App-button--tertiary">
                 Mute
               </button>
             </div>
           </div>
 
-          <div>
+          <div className="App-section">
             <div>Conference call in progress</div>
             <div>+13125551111</div>
             <div>+ Agent Name</div>
             <div>+ Agent Name</div>
             <div>
-              <button type="button" className="App-button">
+              <button type="button" className="App-button App-button--danger">
                 Hangup
               </button>
-              <button type="button" className="App-button">
+              <button type="button" className="App-button App-button--tertiary">
                 Mute
               </button>
             </div>
