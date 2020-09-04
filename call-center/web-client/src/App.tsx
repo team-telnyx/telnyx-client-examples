74a04fa82e9ca30e4a9e919b3bb94e9937262cc7
Deivid Veloso <veloso.deivid@gmail.com>
2020-09-04 09:49:39 -0300

    added error message on Login

--- call-center/web-client/src/App.tsx
+++ call-center/web-client/src/App.tsx
@@ -1,142 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Login from './components/Login';
 import 'normalize.css';
 import './App.css';
+import Common from './components/Common';
 
 function App() {
-  const isLoggedin = false;
+  const [user, setUser] = useState({ isLoggedin: false, error: '' });
+
   return (
     <main className="App">
-      <Login></Login>
-
-      {/* <section className="App-section">
-          <h2 className="App-heading App-headline">Other available agents</h2>
-
-          <ul className="App-agentList">
-            <li className="App-agentList-item">
-              <div>Agent Name</div>
-              <div>
-                <button
-                  type="button"
-                  className="App-button App-button--secondary"
-                >
-                  Transfer
-                </button>
-                <button
-                  type="button"
-                  className="App-button App-button--secondary"
-                >
-                  Add
-                </button>
-              </div>
-            </li>
-            <li className="App-agentList-item">
-              <div>Agent Name</div>
-              <div>
-                <button
-                  type="button"
-                  className="App-button App-button--secondary"
-                >
-                  Transfer
-                </button>
-                <button
-                  type="button"
-                  className="App-button App-button--secondary"
-                >
-                  Add
-                </button>
-              </div>
-            </li>
-            <li className="App-agentList-item">
-              <div>Agent Name</div>
-              <div>
-                <button
-                  type="button"
-                  className="App-button App-button--secondary"
-                >
-                  Transfer
-                </button>
-                <button
-                  type="button"
-                  className="App-button App-button--secondary"
-                >
-                  Add
-                </button>
-              </div>
-            </li>
-          </ul>
-        </section>
-
-        <section className="App-section">
-          <form className="App-form">
-            <label className="App-input-label" htmlFor="phone_number_input">
-              Phone number
-            </label>
-            <div className="App-form-row">
-              <input
-                id="phone_number_input"
-                className="App-input"
-                name="phone_number"
-                type="text"
-              />
-              <button type="submit" className="App-button App-button--primary">
-                Call
-              </button>
-            </div>
-          </form>
-        </section> */}
-
-      {/* <section>
-          <div className="App-section">
-            <div>Incoming call</div>
-            <div className="App-callState-phoneNumber">+13125551111</div>
-            <div className="App-callState-actions">
-              <button type="button" className="App-button App-button--primary">
-                Answer
-              </button>
-              <button type="button" className="App-button App-button--danger">
-                Hangup
-              </button>
-            </div>
-          </div>
-
-          <div className="App-section">
-            <div>Call in progress</div>
-            <div className="App-callState-phoneNumber">+13125551111</div>
-            <div className="App-callState-actions">
-              <button type="button" className="App-button App-button--danger">
-                Hangup
-              </button>
-              <button type="button" className="App-button App-button--tertiary">
-                Mute
-              </button>
-            </div>
-          </div>
-
-          <div className="App-section">
-            <div>Conference call in progress</div>
-            <div className="App-callState-phoneNumber">+13125551111</div>
-            <div>
-              <div className="App-heading App-callState-agentsList-heading">
-                Agents on call
-              </div>
-              <ul className="App-callState-agentList">
-                <li className="App-callState-agentList-item">Agent Name</li>
-                <li className="App-callState-agentList-item">Agent Name</li>
-              </ul>
-            </div>
-            <div className="App-callState-actions">
-              <button type="button" className="App-button App-button--danger">
-                Hangup
-              </button>
-              <button type="button" className="App-button App-button--tertiary">
-                Mute
-              </button>
-            </div>
-          </div>
-        </section> */}
-
-      {isLoggedin && (
+      <Login user={user} onLogin={setUser}></Login>
+      <Common></Common>
+      {user.isLoggedin && (
         <footer>
           <button type="button" className="App-button App-button--link">
             Logout
--- call-center/web-client/src/components/Login.tsx
+++ call-center/web-client/src/components/Login.tsx
@@ -1,47 +1,60 @@
-import React, { Component, FormEvent } from 'react';
-import PropTypes from 'prop-types';
+import React, { FormEvent } from 'react';
 import './Login.css';
-import { AxiosError, AxiosResponse } from 'axios';
+import { AxiosError } from 'axios';
 import { login } from '../services/loginService';
+import { User } from '../interfaces/User';
 
-class Login extends Component {
-  handleSubmit = async (event: FormEvent) => {
+function Login({ user, onLogin }: ILogin) {
+  const handleSubmit = async (event: FormEvent) => {
     event.preventDefault();
+
     const result = await login('Hugo Oliveira');
+
     if ('data' in result) {
       console.log('Success');
+      onLogin({ ...user, isLoggedin: true });
     }
-    console.log(
-      `Failed ${(result as AxiosError).response?.data?.error?.message}`
-    );
+
+    onLogin({
+      ...user,
+      isLoggedin: true,
+      error: `Failed ${(result as AxiosError).response?.data?.error?.message}`,
+    });
   };
 
-  render() {
-    return (
-      <section className="Login">
-        <header>
-          <h1 className="App-heading App-title">Call Center Login</h1>
-        </header>
+  return (
+    <section className="Login">
+      <header>
+        <h1 className="App-heading App-title">Call Center Login</h1>
+      </header>
 
-        <form className="App-form" onSubmit={this.handleSubmit}>
-          <label className="App-input-label" htmlFor="display_name_input">
-            Name
-          </label>
-          <div className="App-form-row">
-            <input
-              id="display_name_input"
-              className="App-input"
-              name="display_name"
-              type="text"
-              value="Hugo Oliveira"
-            />
-            <button type="submit" className="App-button App-button--primary">
-              Login
-            </button>
-          </div>
-        </form>
-      </section>
-    );
-  }
+      <form className="App-form" onSubmit={handleSubmit}>
+        <label className="App-input-label" htmlFor="display_name_input">
+          Name
+        </label>
+        <div className="App-form-row">
+          <input
+            id="display_name_input"
+            className="App-input"
+            name="display_name"
+            type="text"
+            value="Hugo Oliveira"
+          />
+          <button type="submit" className="App-button App-button--primary">
+            Login
+          </button>
+        </div>
+      </form>
+      {user && user.error && user.error.length > 0 && (
+        <p className="Login-error">{user.error}</p>
+      )}
+    </section>
+  );
 }
+
+interface ILogin {
+  user: User;
+  onLogin: Function;
+}
+
 export default Login;
