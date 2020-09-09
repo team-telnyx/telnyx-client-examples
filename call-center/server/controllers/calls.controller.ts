8d4db4ed93b94705c0ce2a98c8c1d01874012ce2
Sua Yoo <sua@telnyx.com>
2020-09-09 16:35:03 -0700

    Refactor call center Call Control flow (#12)

--- call-center/server/controllers/calls.controller.ts
+++ call-center/server/controllers/calls.controller.ts
@@ -7,12 +7,17 @@ let telnyxPackage: any = require('telnyx');
 
 let telnyx = telnyxPackage(process.env.TELNYX_API_KEY);
 
-// NOTE I don't think we need to dynamically
-// encode call client state values so they're
-// hardcoded right now. The CC API expects
-// base64 encdoed values.
+// The CC API expects base64 encdoed values
+const base64Encode = (data: string) => {
+  let buffer = new Buffer(data);
+
+  return buffer.toString('base64');
+};
+
 const ENCODED_CALL_CLIENT_STATE = {
-  HANGUP: 'Y2FsbC5oYW5ndXA=',
+  HANGUP: base64Encode('hangup'),
+  BRIDGING: base64Encode('bridging'),
+  BRIDGED: base64Encode('bridged'),
 };
 
 class CallsController {
@@ -36,16 +41,16 @@ class CallsController {
     try {
       switch (event.data.event_type) {
         case 'call.initiated':
-          await CallsController.handleCallInitiated(event);
+          await CallsController.handleInitiated(event);
           break;
         case 'call.answered':
-          await CallsController.handleCallAnswered(event);
+          await CallsController.handleAnswered(event);
           break;
         case 'call.hangup':
-          await CallsController.handleCallHangup(event);
+          await CallsController.handleHangup(event);
           break;
         case 'call.speak.ended':
-          await CallsController.handleCallSpeakEnded(event);
+          await CallsController.handleSpeakEnded(event);
           break;
       }
     } catch (e) {
@@ -70,11 +75,7 @@ class CallsController {
     return findFirst();
   };
 
-  private static handleCallInitiated = async function (event: any) {
-    let telnyxCall = new telnyx.Call({
-      connection_id: process.env.TELNYX_CC_APP_ID,
-      call_control_id: event.data.payload.call_control_id,
-    });
+  private static handleInitiated = async function (event: any) {
     let callRepository = getManager().getRepository(Call);
 
     let {
@@ -89,8 +90,6 @@ class CallsController {
     /*
      * Only answering parked calls because we also get the call.initiated
      * event for the second leg of the call
-     *
-     * Transfer to agent is handled in the `call.answered` event handler
      */
     if (state === 'parked') {
       let call = new Call();
@@ -102,64 +101,64 @@ class CallsController {
 
       await callRepository.save(call);
 
-      telnyxCall.answer();
+      CallsController.transferParkedCall(event);
     }
   };
 
-  private static handleCallAnswered = async function (event: any) {
-    let {
-      call_control_id,
-      call_session_id,
-      call_leg_id,
-      to,
-      from,
-    } = event.data.payload;
+  private static transferParkedCall = async function (event: any) {
+    let callRepository = getManager().getRepository(Call);
+    let { call_session_id, call_leg_id } = event.data.payload;
+
+    let call = await callRepository.findOneOrFail({
+      callSessionId: call_session_id,
+      callLegId: call_leg_id,
+    });
 
     let telnyxCall = new telnyx.Call({
-      connection_id: process.env.TELNYX_CC_APP_ID,
       call_control_id: event.data.payload.call_control_id,
     });
 
-    let callRepository = getManager().getRepository(Call);
+    await telnyxCall.answer({
+      client_state: ENCODED_CALL_CLIENT_STATE.BRIDGING,
+    });
 
-    // FIXME More robust way to handle whether call is being answered by
-    // this app (immediately after "parked" state) or by agent's SIP client?
-    if (to.startsWith('sip:')) {
-      console.log('call answered by agent');
+    let availableAgent = await CallsController.getAvailableAgent();
+
+    if (availableAgent) {
+      await telnyxCall.transfer({
+        to: `sip:${availableAgent.sipUsername}@sip.telnyx.com`,
+        // NOTE Client state only persists on original leg of
+        // the call (not the one we're transferring to)
+        client_state: ENCODED_CALL_CLIENT_STATE.BRIDGED,
+      });
+
+      availableAgent.available = false;
+      call.agents = [availableAgent];
+
+      callRepository.save(call);
     } else {
-      let availableAgent = await CallsController.getAvailableAgent();
-
-      if (availableAgent) {
-        await telnyxCall.transfer({
-          to: `sip:${availableAgent.sipUsername}@sip.telnyx.com`,
-        });
-
-        availableAgent.available = false;
-
-        // FIXME Does this need to be a new call?
-        let call = new Call();
-        call.callSessionId = call_session_id;
-        call.callControlId = call_control_id;
-        call.callLegId = call_leg_id;
-        call.to = to;
-        call.from = from;
-        call.agents = [availableAgent];
-
-        callRepository.save(call);
-      } else {
-        telnyxCall.speak({
-          // Let our app know we should hangup after call ends
-          client_state: ENCODED_CALL_CLIENT_STATE.HANGUP,
-          // All following fields are required:
-          language: 'en-US',
-          payload: 'Sorry, there are no agents available to take your call.',
-          voice: 'female',
-        });
-      }
+      telnyxCall.speak({
+        // Let our app know we should hangup after call ends
+        client_state: ENCODED_CALL_CLIENT_STATE.HANGUP,
+        // All following fields are required:
+        language: 'en-US',
+        payload: 'Sorry, there are no agents available to take your call.',
+        voice: 'female',
+      });
     }
   };
 
-  private static handleCallSpeakEnded = async function (event: any) {
+  private static handleAnswered = async function (event: any) {
+    let { client_state } = event.data.payload;
+
+    if (client_state === ENCODED_CALL_CLIENT_STATE.BRIDGING) {
+      console.log('call answered by app\n');
+    } else {
+      console.log('call answered by agent\n');
+    }
+  };
+
+  private static handleSpeakEnded = async function (event: any) {
     let { client_state } = event.data.payload;
 
     if (client_state === ENCODED_CALL_CLIENT_STATE.HANGUP) {
@@ -174,11 +173,7 @@ class CallsController {
   /*
    * Cleanup agents and call relation on hangup
    */
-  private static handleCallHangup = async function (event: any) {
-    let telnyxCall = new telnyx.Call({
-      connection_id: process.env.TELNYX_CC_APP_ID,
-      call_control_id: event.data.payload.call_control_id,
-    });
+  private static handleHangup = async function (event: any) {
     let callRepository = getManager().getRepository(Call);
     let { call_session_id, call_leg_id } = event.data.payload;
 
