c1b21a2f1c0958b0fa70a02869558515df3fdb03
Sua Yoo <sua@telnyx.com>
2020-09-10 10:42:41 -0700

    Add WebRTC lib to client (#14)

--- call-center/web-client/src/components/Common.tsx
+++ call-center/web-client/src/components/Common.tsx
@@ -1,7 +1,71 @@
-import React from 'react';
-function Common() {
+import React, { useEffect, useRef, useState } from 'react';
+import { TelnyxRTC } from '@telnyx/webrtc';
+
+interface ICommon {
+  // User's WebRTC JWT
+  token: string;
+}
+
+function Common({ token }: ICommon) {
+  const telnyxClientRef = useRef<any>();
+  // Check if component is mounted before updating state
+  // in TelnyxRTC callbacks
+  const isMountedRef = useRef<any>(null);
+  let [webRTCState, setWebRTCState] = useState<string>('');
+
+  const updateWebRTCState = (state: string) => {
+    if (isMountedRef.current) {
+      setWebRTCState(state);
+    }
+  };
+
+  useEffect(() => {
+    isMountedRef.current = true;
+
+    const telnyxClient = new TelnyxRTC({
+      // Required credentials
+      login_token: token,
+    });
+
+    telnyxClient.on('telnyx.ready', () => {
+      console.log('ready');
+
+      updateWebRTCState('ready');
+    });
+
+    telnyxClient.on('telnyx.error', (error: any) => {
+      console.error('error:', error);
+
+      updateWebRTCState('error');
+    });
+
+    telnyxClient.on('telnyx.socket.close', () => {
+      console.log('close');
+
+      telnyxClient.disconnect();
+
+      updateWebRTCState('disconnected');
+    });
+
+    telnyxClient.on('telnyx.notification', (notification: any) => {
+      console.log('notification:', notification);
+    });
+
+    telnyxClientRef.current = telnyxClient;
+    telnyxClientRef.current.connect();
+
+    return () => {
+      isMountedRef.current = false;
+
+      telnyxClientRef.current.disconnect();
+      telnyxClientRef.current = undefined;
+    };
+  }, [token]);
+
   return (
     <div>
+      <section className="App-section">WebRTC status: {webRTCState}</section>
+
       <section className="App-section">
         <h2 className="App-heading App-headline">Other available agents</h2>
 
