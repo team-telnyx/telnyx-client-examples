ca769d5786068e279ec94725ef94dffd5f4f260f
Sua Yoo <sua@telnyx.com>
2020-09-28 15:06:25 -0700

    add setup and prestart scripts

---
+++ call-center/server/scripts/prestart.js
@@ -0,0 +1,24 @@
+/* Run configs before each startup */
+const telnyx = require('telnyx')(process.env.TELNYX_API_KEY);
+
+async function prestart() {
+  if (!process.env.NGROK_SUBDOMAIN) {
+    return;
+  }
+
+  try {
+    const {
+      data: callControlApp,
+    } = await telnyx.callControlApplications.retrieve(
+      process.env.TELNYX_CC_APP_ID
+    );
+
+    await callControlApp.update({
+      webhook_event_url: `https://${process.env.NGROK_SUBDOMAIN}.ngrok.io/calls/callbacks/call-control-app`,
+    });
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+prestart();
