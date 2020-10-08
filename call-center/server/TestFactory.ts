091f19f550a250348026a69b65b7fd06d3b60e7d
Sua Yoo <sua@telnyx.com>
2020-10-08 13:42:37 -0700

    Add server test fixtures (#40)

--- call-center/server/TestFactory.ts
+++ call-center/server/TestFactory.ts
@@ -1,6 +1,14 @@
+import path from 'path';
 import express from 'express';
 import request from 'supertest';
-import { Connection, createConnection } from 'typeorm';
+import { Connection, createConnection, getRepository } from 'typeorm';
+import {
+  Builder,
+  fixturesIterator,
+  Loader,
+  Parser,
+  Resolver,
+} from 'typeorm-fixtures-cli';
 import createApp from './createApp';
 
 /* Set up and tear down server and db for tests */
@@ -16,20 +24,58 @@ class TestFactory {
     return this._connection;
   }
 
+  /*
+   * Create app and db connection
+   *
+   * Usage:
+   *  beforeAll(async () => {
+   *    await testFactory.init();
+   *  });
+   */
   public async init() {
     this._connection = await createConnection({
       type: 'sqljs',
       database: new Uint8Array(),
       location: 'database',
       entities: ['**/entities/*.ts'],
+      dropSchema: true,
       synchronize: true,
       logging: false,
     });
     this._app = createApp();
   }
 
-  public async destroy() {
+  /*
+   * Clean up app and db connection
+   *
+   * Usage:
+   *  afterAll(async () => {
+   *    await testFactory.close();
+   *  });
+   */
+  public async close() {
     this._connection?.close();
+  }
+
+  /*
+   * Load test data
+   */
+  public async loadFixtures() {
+    try {
+      const loader = new Loader();
+      loader.load(path.resolve('./fixtures'));
+
+      const resolver = new Resolver();
+      const fixtures = resolver.resolve(loader.fixtureConfigs);
+      const builder = new Builder(this.connection!, new Parser());
+
+      for (const fixture of fixturesIterator(fixtures)) {
+        const entity: any = await builder.build(fixture);
+        await getRepository(entity.constructor.name).save(entity);
+      }
+    } catch (err) {
+      console.log(err);
+    }
   }
 }
 
