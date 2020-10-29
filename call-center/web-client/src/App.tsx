72f20d09d25f3e113a8d7ef2ffbed69d2b8f0917
Deivid Veloso <veloso.deivid@gmail.com>
2020-10-29 11:03:57 -0300

    remove console.log

--- call-center/web-client/src/App.tsx
+++ call-center/web-client/src/App.tsx
@@ -14,7 +14,6 @@ interface ISessionStorageUser {
 }
 
 function App() {
-  console.log('process.env.REACT_APP_SIP_DOMAIN',process.env.REACT_APP_SIP_DOMAIN)
   const [sessionStorageUser, setSessionStorageUser] = useSessionStorage<
     ISessionStorageUser
   >('call_center_user', {});
