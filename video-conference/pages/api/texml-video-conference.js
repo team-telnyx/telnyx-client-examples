c4afbc4a3d73a6d6071d363312e0d93fc3962554
Hugo Bessa <eu@hugobessa.com.br>
2020-12-14 12:44:49 -0300

    Add Conference Params

--- video-conference/pages/api/texml-video-conference.js
+++ video-conference/pages/api/texml-video-conference.js
@@ -1,4 +1,7 @@
 export default (req, res) => {
+  if (!req.method === "POST") {
+    throw new Error("Your TeXML Application should send a POST webhook");
+  }
   if (!req.body.To) {
     throw new Error("No To in the Body, cannot complete call");
   }
@@ -6,7 +9,7 @@ export default (req, res) => {
   let xmlResponse = `<?xml version="1.0" encoding="UTF-8"?>
   <Response>
       <Dial>
-          <Conference>Video Conference ${req.body.To}</Conference>
+          <Conference videoLayoutName="group:grid_zoom" statusCallback="https://e95c25777e39.ngrok.io/api/log" statusCallbackEvent="start,end,join,leave">Video Conference ${req.body.To}</Conference>
       </Dial>
   </Response>`;
   res.send(xmlResponse);
