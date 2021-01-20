8487f755eed2b25ebc516ad8538b57b14543faf5
Sua Yoo <sua@telnyx.com>
2021-01-20 15:29:36 -0800

    setup oauth

--- one-to-one-video/pages/index.js
+++ one-to-one-video/pages/index.js
@@ -1,18 +1,39 @@
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
+import { useSession, signIn } from 'next-auth/client';
+import { Box, Button } from 'grommet';
+import { Github } from 'grommet-icons';
 import useCachedToken from '../utils/useCachedToken';
 import Page from '../components/Page';
 
 export default function Home() {
   const router = useRouter();
+  const [session, loading] = useSession();
   const [token] = useCachedToken();
 
-  useEffect(() => {
-    if (token === null) {
-      // Redirect to login page
-      router.push('/login');
-    }
-  }, [token]);
+  // useEffect(() => {
+  //   if (token === null) {
+  //     // Redirect to login page
+  //     router.push('/login');
+  //   }
+  // }, [token]);
 
-  return <Page title="Home">Video</Page>;
+  console.log(session);
+
+  return (
+    <Page title="Home">
+      {session && <p>Signed in as {session.user.name}</p>}
+      {!session && !loading && (
+        <Box width="medium">
+          <Button
+            primary
+            size="large"
+            icon={<Github />}
+            label="Sign in with GitHub"
+            onClick={() => signIn('github')}
+          />
+        </Box>
+      )}
+    </Page>
+  );
 }
